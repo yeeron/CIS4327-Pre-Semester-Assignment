3db1fb12155d3d6a0c17a970a47448ad51c330aa
BIGDummyHead <codestopping22@gmail.com>
2026-07-23 11:49:50 -0400

    add in login controller and featured functions

---
+++ VolunteerMS/Controllers/AccountController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using VolunteerMS.Data;
+using VolunteerMS.Models;
+
+namespace VolunteerMS.Controllers
+{
+    [AllowAnonymous]
+    public class AccountController : Controller
+    {
+
+        private readonly AppDbContext _context;
+        private readonly PasswordHasher<User> _pwHasher;
+
+        public AccountController(AppDbContext context)
+        {
+            _context = context;
+            _pwHasher = new PasswordHasher<User>();
+        }
+        
+        /// Returns the login page for the user to enter their credentials.
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// Sets the model state error, keep it vauge 
+        /// </summary>
+        private void DenyAccess()
+        {
+            ModelState.AddModelError("", "Invalid username or password.");
+            //can do more here to deny access if needed.
+        }
+
+        private async Task<IActionResult> ClaimUser(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Username),
+            };
+
+            var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+            var principal = new ClaimsPrincipal(identity);
+
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, principal);
+
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        // Logs in the user and validates them. Gives back a cookie to their system to allow them to access other pages in the system.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(string username, string password)
+        {
+
+            var user = _context.Users.FirstOrDefault(u => u.Username == username);
+
+
+            // is invalid user?
+            // user was null or password hashing mismatch
+            if (user == null || _pwHasher.VerifyHashedPassword(user, user.PasswordHash, password) == PasswordVerificationResult.Failed)
+            {
+                // kickback to login
+                DenyAccess();
+                return View();
+            }
+
+           
+            return await ClaimUser(user);
+        }
+    }
+}
